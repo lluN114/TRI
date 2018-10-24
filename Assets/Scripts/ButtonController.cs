c3a60ac7b9d4b0be64da76227ef243771e92f4c0
horutosou127 <chuunibyou127@gmail.com>
2018-10-24 12:21:07 +0900

    ぼたんつくった☆

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour {
+
+    public GameManager gameManager;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void ReturnButtonName()
+    {
+        int play_button;
+        switch (transform.name)
+        {
+            case "Button1":
+                play_button = 1;
+                break;
+            case "Button2":
+                play_button = 2;
+                break;
+            case "Button3":
+                play_button = 3;
+                break;
+            default:
+                play_button = -1;
+                break;
+        }
+
+        //Debug.Log(play_button);
+        gameManager.SetSelectElectric(play_button);
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public ButtonController buttonController;
+    public int select_electric;
+
+    public Button[] buttons;
+
+    // Use this for initialization
+    void Start ()
+    {
+        select_electric = -1;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        Debug.Log(select_electric);
+    }
+
+    public void SetSelectElectric(int select_ele)
+    {
+        select_electric = select_ele;
+    }
+    
 }
