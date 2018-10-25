67ebf27ba53911cac962247b387674413492ae10
春翔 <nkc.shota.kasugai@gmail.com>
2018-10-25 10:59:31 +0900

    仮タイトル＋シーン遷移とか

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -1,8 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneTransition : MonoBehaviour {
+
+    public void Trans(string s)
+    {
+        SceneManager.LoadScene(s);
+    }
+
+    public void OverlaidTrans(string s)
+    {
+        SceneManager.LoadScene(s, LoadSceneMode.Additive);
+    }
 
 	// Use this for initialization
 	void Start () {
---
+++ Assets/Scripts/Title/PieceButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PieceButton : BaseButton
+{
+    public SceneTransition t;
+
+    protected override void OnClick(string objectName)
+    {
+        if ("start".Equals(objectName))
+        {
+            // Button1がクリックされたとき
+            t.Trans("GameScene");
+            Debug.Log("start");
+        }
+        else if ("exit".Equals(objectName))
+        {
+            // Button2がクリックされたとき
+            Debug.Log("exit");
+            Application.Quit();
+        }
+        else
+        {
+            throw new System.Exception("Not implemented!!");
+        }
+    }
+}
