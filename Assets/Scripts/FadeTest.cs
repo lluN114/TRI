06fa8e8b1667288d51bc2922e5a9c58f9a430643
horutosou127 <chuunibyou127@gmail.com>
2018-11-01 10:47:01 +0900

    トランジション

---
+++ Assets/Scripts/FadeTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        FadeManager.FadeIn();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+            FadeManager.FadeOut(1);
+    }
+}
