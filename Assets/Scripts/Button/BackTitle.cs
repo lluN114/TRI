a2aa1372c851567de1b460e74c02169db2f5a2d9
horutosou127 <chuunibyou127@gmail.com>
2019-01-24 12:22:46 +0900

    ボタンスクリプト付与

---
+++ Assets/Scripts/Button/BackTitle.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackTitle : MonoBehaviour {
+
+    public void OnClick()
+    {
+        FadeManager.FadeOut(1);
+    }
+}
